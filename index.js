2db158fc98b701be459bc4dc79dccd5b6b35bca2
Vũ Đạt <74701328+dat911zz@users.noreply.github.com>
2023-05-14 09:44:10 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
+var petList = [
+  {
+    "id": 1,
+    "type": "dog",
+    "price": 249.99
+  },
+  {
+    "id": 2,
+    "type": "cat",
+    "price": 124.99
+  },
+  {
+    "id": 3,
+    "type": "fish",
+    "price": 0.99
+  }
+]
 
-  module.exports = app;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get("/pets", (req, res, next) => {
+  res.json(petList);
+});
+app.get("/pets/:id", (req, res, next) => {
+  res.json(petList[req.params.id]);
+});
+
+
+module.exports = app;
